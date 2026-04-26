748fe3034bfcee500300d15fd6051f8946249400
GS QUOTZ <gsquotz@gmail.com>
2026-04-26 20:33:21 +0900

    Update server.js
    duplicado remov

--- server.js
+++ server.js
@@ -321,9 +321,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("Servidor rodando na porta " + PORT);
-});
+
 
 /* ========================
    START
